8bb24e07dcb2924d675ae658cc8f7493838382d6
kinosh <kinosh@mercari.com>
2024-04-12 15:23:05 +0900

    demo

---
+++ demo.js
@@ -0,0 +1,40 @@
+let aa = 1;
+
+aa = aa + 1;
+
+console.log(aa)
+
+console.log('出力');
+
+var name;
+name = 'kinosh'
+
+var name = 'kinosh';
+
+var name = 'kinosh';
+name = 'Yumi'; 
+console.log(name); //Yumi
+
+var name1, name2;
+
+
+var str = 'string'; //文字列
+var num = 100; //数値　
+var arr = ['a', 'b']; //配列
+var obj = {a: 'b'}; //オブジェクト
+var fun = function() {}; //関数
+
+var name = '山田太郎';
+
+var name = '山田' + '太郎';
+console.log(name); //山田太郎
+
+var lastname = '山田';
+var firstname = '一郎';
+var fullname = lastname + firstname;
+console.log(fullname); //山田一郎
+
+var x = 10;
+
+var result = 10 + 44;
+console.log(result);
