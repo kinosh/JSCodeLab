873bbae65372734d0512c854700a1a1b8aa4429c
kinosh <kinosh@mercari.com>
2024-04-23 15:46:58 +0900

    update0423

--- demo.js
+++ demo.js
@@ -38,3 +38,60 @@ var x = 10;
 
 var result = 10 + 44;
 console.log(result);
+
+var fruit1 = 'みかん';
+var fruit2 = 'いちご';
+var fruit3 = 'りんご';
+
+var fruit = ['みかん','ぶどう','りんご'];
+var orange = fruit[0];
+var grape = fruit[1];
+var apple = fruit[2];
+console.log(orange); //みかん
+console.log(grape); //ぶどう
+console.log(apple); //りんご
+
+console.log(fruit.length)
+
+
+var fruit = ['みかん', 'りんご', 'ぶどう'];
+var orange = fruit[0];
+var apple = fruit[1];
+var grape = fruit[2];
+console.log(orange) // みかん
+console.log(apple) // りんご
+console.log(grape) // ぶどう
+
+console.log(fruit.length);
+
+var fruit = ['みかん', 'りんご', 'ぶどう'];
+var lastIndex = fruit.length - 1;
+var lastElement = fruit[lastIndex];
+console.log(lastIndex) //わからない
+
+var personName = '山田一郎';
+var personAge = 20;
+var personGender = 'male';
+
+var person = {
+    name: '山田一郎',
+    age: 20,
+    gender: 'male'
+};
+
+var name = person.name
+console.log(name);
+
+var person = {
+    name: '山田太郎',
+    age: 20,
+    gender: 'male',
+    address: {
+    postcode: '100-0001',
+    pref: '東京都',
+    city: '千代田区'
+     }
+    };
+    var postcode = person.address.postcode;
+    console.log(postcode); //100-0001
+
---
+++ demo2.js
@@ -0,0 +1,10 @@
+var person = {
+    name: '山田太郎',
+    age: 20,
+    gender: 'male',};
+person.age = 21;
+console.log(person.age);
+
+person.kana = 'やまだたろう'
+console.log(person.kana);
+
